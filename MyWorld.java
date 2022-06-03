0a05edf22a59735cdfe8ea7e7b2e52023b476de6
Nathan-yat-Chan <340763200@gapps.yrdsb.ca>
2022-06-03 10:14:44 -0400

    added mines and numbers for minesweeper

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,17 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        Label mine = new Label("BOOM", 20);
+        for (int i=0;i<6;i++)
+        {
+            
+        }
+        Label one = new Label("1", 40);
+        Label two = new Label("2", 40);
+        Label three = new Label("3", 40);
+        Label four = new Label("4", 40);
+        Label five = new Label("5", 40);
+        Label six = new Label("6", 40);
+        Label seven = new Label("7", 40);
     }
 }
