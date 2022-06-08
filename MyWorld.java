487a1bafe9250b3794d5b9b9282f5b27d8957ac0
Chan <340763200@EDU.YRDSB.NET>
2022-06-08 12:05:48 -0400

    figuring out how to make multiple mines

--- MyWorld.java
+++ MyWorld.java
@@ -20,21 +20,38 @@ public class MyWorld extends World
         int[] xarr = {(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104};
         int[] yarr = {(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64};
         Label mine = new Label("BOOM", 20);
-        for (int i=0;i<6;i++)
-        {
-            addObject(mine, yarr[i], xarr[i]);
-        }
-        int[] numx = {104,208,312,416,520};
-        int[] numy = {64,128,192,256,320};
-        for (int i=0;i<25;i++)
-        {
-            
-        }
+        counter2 plate = new counter2(" ");
         Counter one = new Counter("1");
         Counter two = new Counter("2");
         Counter three = new Counter("3");
         Counter four = new Counter("4");
         Counter five = new Counter("5");
         Counter six = new Counter("6");
+        for (int i=0;i<6;i++)
+        {
+            addObject(mine, yarr[i], xarr[i]);
+        }
+        int[] numx = {104,208,312,416,520};
+        int[] numy = {64,128,192,256,320};
+        for (int i=0;i<5;i++)
+        {
+            addObject(plate, 64,numx[i]); 
+        }
+        for (int i=0;i<5;i++)
+        {
+            addObject(plate, 128,numx[i]); 
+        }
+        for (int i=0;i<5;i++)
+        {
+            addObject(plate, 192,numx[i]); 
+        }
+        for (int i=0;i<5;i++)
+        {
+            addObject(plate, 256,numx[i]); 
+        }
+        for (int i=0;i<5;i++)
+        {
+            addObject(plate, 320,numx[i]); 
+        }
     }
 }
