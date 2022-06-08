e3fbe0a595693794ce626766f706ae4fc7e6ae2f
Chan <340763200@EDU.YRDSB.NET>
2022-06-08 10:21:47 -0400

    added mines, but figuring out how to add multiple

--- MyWorld.java
+++ MyWorld.java
@@ -17,17 +17,24 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        int[] xarr = {(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104,(Greenfoot.getRandomNumber(5)+1)*104};
+        int[] yarr = {(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64,(Greenfoot.getRandomNumber(5)+1)*64};
         Label mine = new Label("BOOM", 20);
         for (int i=0;i<6;i++)
         {
+            addObject(mine, yarr[i], xarr[i]);
+        }
+        int[] numx = {104,208,312,416,520};
+        int[] numy = {64,128,192,256,320};
+        for (int i=0;i<25;i++)
+        {
             
         }
-        Label one = new Label("1", 40);
-        Label two = new Label("2", 40);
-        Label three = new Label("3", 40);
-        Label four = new Label("4", 40);
-        Label five = new Label("5", 40);
-        Label six = new Label("6", 40);
-        Label seven = new Label("7", 40);
+        Counter one = new Counter("1");
+        Counter two = new Counter("2");
+        Counter three = new Counter("3");
+        Counter four = new Counter("4");
+        Counter five = new Counter("5");
+        Counter six = new Counter("6");
     }
 }
